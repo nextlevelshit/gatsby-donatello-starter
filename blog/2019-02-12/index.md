96fe684c5541570cfdecc7871bce3f5fa27b1a76
Michael Czechowski <mail@dailysh.it>
2019-02-13 12:14:45 +0100

    little config adaptions

---
+++ blog/2019-02-12/index.md
@@ -0,0 +1,38 @@
+---
+date: "2019-02-12"
+title: "WEB 1.0 - Constant Imperfection"
+category: "Coding"
+tags: ['Technology', 'Coding']
+banner: "/assets/bg/4.jpg"
+---
+
+## tl;dr
+
+*Abstract, gist, summary, essence*
+
+Technology changes the way we interact with the world, communicate to people and finding out who we are and what we want.
+That massivly accelerating progress, that technologies are passing through, might force people to change their way of living or even does already.
+Constantly adapting new knowledge, new social codes and assimilating ourselves to the volatile environment makes us stay busy, imperfect, unfinished and at worst this ongoing effort might take us the chance of stay 
+
+## Introduction
+
+Think of the future.
+What do you see? 
+What comes up to your mind?
+
+Are you imagining yourself with bent posture, increasing wrinkles all over your body and facing a monoton daily routine?
+Or are you thinking of the modern age with flying cars, holographic displays and healthy protein pils for breakfast?
+
+I am not a distopian, I am not evoking the *Brave new World* by Aldous Huxley.
+I am just trying to capture my skeptical view of the on going changes in the world of technology, academic research and communication in an inter-connected world of humans and devices; users and adaptors; progress and regression; acceleration and deceleration.
+
+Hi, I am Michael Czechowski, 30 years old, born in Poland, bilingual in German and Polish, programming web engineer for about 15 years, holding undergraduate degrees in Business Informatics and Philosophy.
+Looking for such a long time on the web, life and humans makes think of a better world and I'd really like to join my journey.
+
+Please, try follow me and my vision.
+Everything that might appear unclear and too brief to you, can be explained comprehensively.
+All you have to do is, to click onto a specific word or paragraph to get a more detailed version of what I am trying to tell you.
+You will find explanations, links to articles or short summaries of certain topics in this article.
+
+
+mental clearity, bus stop, constant change, disruption, business model
--- config/SiteConfig.ts
+++ config/SiteConfig.ts
@@ -1,21 +1,21 @@
 export default {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Typescript Power Blog', // Navigation and Site Title
-  siteTitleAlt: 'Typescript Power Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://typescript-power-blog.github.com', // Domain of your site. No trailing slash!
+  siteTitle: 'SARAH ESSER', // Navigation and Site Title
+  siteTitleAlt: 'SARAH ESSER - Artist and Sculptor from Berlin, Paris', // Alternative Site title for SEO
+  siteUrl: 'http://sarahesser.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/assets/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   defaultBg: '/assets/bg.png', // default post background header
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Typescript Power Blog with big typography', // Your site description
-  author: 'Majid Hajian', // Author for schemaORGJSONLD
+  siteDescription: `Sarah Esser, born 1977 in Münster, lives and works in Berlin as an artist and sculptor. As a „Meisterschülerin“ of Prof. Berndt Wilde (Kunsthochschule Berlin) she received several prizes and scholarships in Europe. Her oeuvre comprises more than 100 sculptures, drawings and watercolors.`, // Your site description
+  author: 'Sarah Esser', // Author for schemaORGJSONLD
   siteLogo: '/assets/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@mhadaily', // Twitter Username - Optional
-  ogSiteName: 'mhadaily', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
+  userTwitter: '', // Twitter Username - Optional
+  ogSiteName: '', // Facebook Site Name - Optional
+  ogLanguage: '', // Facebook Language
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
@@ -26,9 +26,6 @@ export default {
   headerFontFamily: 'Bitter',
   bodyFontFamily: 'Open Sans',
   baseFontSize: '18px',
-
-  // Social media
-  siteFBAppID: '',
 
   //
   Google_Tag_Manager_ID: '',
